b5d6760c1e00bcc24cf70e3d0ccff062e8431bbf
Piotr Wasak <piotr.wasak@currenda.pl>
2022-04-30 14:43:07 +0200

    add

--- app/src/main/java/com/example/pamoapp/MainActivity.java
+++ app/src/main/java/com/example/pamoapp/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.pamoapp;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -13,6 +15,8 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.example.pamoapp.databinding.ActivityMainBinding;
+
+import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -34,10 +38,22 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
+
+        EditText editTextHeight = (EditText) findViewById(R.id.editTextNumber);
+        Log.i("info", editTextHeight.getText().toString());
     }
 
+
     public void clickCalc(View view) {
-        Log.i("info", "Btn pressed");
+        EditText editTextHeight = (EditText) findViewById(R.id.editTextNumber);
+        EditText editTextWeight = (EditText) findViewById(R.id.editTextNumber2);
+        TextView calculatedText = (TextView) findViewById(R.id.textView3);
+
+        double heightVal = Integer.parseInt(editTextHeight.getText().toString());
+        double weightVal = Integer.parseInt(editTextWeight.getText().toString());
+        double heightInMeter = heightVal/100;
+        double bmi = (weightVal/Math.pow(heightInMeter, 2));
+        calculatedText.setText(new DecimalFormat("#.##").format(bmi));
     }
 
 }
--- app/src/main/java/com/example/pamoapp/ui/home/HomeViewModel.java
+++ app/src/main/java/com/example/pamoapp/ui/home/HomeViewModel.java
@@ -2,6 +2,7 @@ package com.example.pamoapp.ui.home;
 
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
@@ -13,15 +14,6 @@ public class HomeViewModel extends ViewModel {
 
     public HomeViewModel() {
         mText = new MutableLiveData<>();
-//        mText.setValue("Test");
-    }
-
-    public double calculateBMI(double height, double weight) {
-        return weight/(Math.pow(height, 2));
-    }
-
-    public void clickCalc(View view) {
-        Log.i("info", "Btn pressed");
     }
 
     public LiveData<String> getText() {
